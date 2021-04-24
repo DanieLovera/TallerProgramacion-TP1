09cf99dcd77d962491b346d5553faaf95f6d7fbd
Daniel Lovera <daanilo2497@gmail.com>
2021-04-23 23:10:06 -0300

    Corrigo errores y agrego información de debug

--- socket.c
+++ socket.c
@@ -12,14 +12,17 @@
 #define SERVER_SIDE_FLAGS AI_PASSIVE
 #define CLIENT_SIDE_FLAGS 0
 
-int _socket_is_in_valid_state(socket_t *self);
-int _socket_addrinfo_iterate(socket_t *self, struct addrinfo *result);
-int _socket_create_and_connect(socket_t *self, struct addrinfo **result);
-int _socket_get_addrinfo(socket_t *self, 
-						  const char *host, 
-						  const char *service,
-						  const int FLAGS, 
-						  struct addrinfo **result);
+#define DEBUG_INFO
+#define DEBUG_PRINT(MSG, VALUE) fprintf(stderr, MSG, VALUE);
+
+static int _socket_is_in_valid_state(socket_t *self);
+static int _socket_addrinfo_iterate(socket_t *self, struct addrinfo *result);
+static int _socket_create_and_connect(socket_t *self, struct addrinfo **result);
+static int _socket_get_addrinfo(socket_t *self, 
+							    const char *host, 
+						  		const char *service,
+						  		const int FLAGS, 
+						  		struct addrinfo **result);
 
 
 void socket_init(socket_t *self) {
@@ -29,7 +32,15 @@ void socket_init(socket_t *self) {
 void socket_uninit(socket_t *self) {
 	if (_socket_is_in_valid_state(self)) {
 		shutdown(self->file_descriptor, SHUT_RDWR);
+		#ifdef DEBUG_INFO
+		DEBUG_PRINT("'shutdown' in socket_uninit:socket.c: %s\n", 
+					 strerror(errno))
+		#endif
 		close(self->file_descriptor);
+		#ifdef DEBUG_INFO
+		DEBUG_PRINT("'close' in socket_uninit:socket.c: %s\n", 
+					 strerror(errno))
+		#endif
 	}
 }
 
@@ -44,8 +55,11 @@ void socket_accept(socket_t *listener, socket_t *peer) {
 
 int socket_connect(socket_t *self, const char *host, const char *service) {
 	struct addrinfo *result = NULL;
-	int rt_code = 0;
-	rt_code = _socket_get_addrinfo(self, host, service, CLIENT_SIDE_FLAGS, &result);
+	int rt_code = _socket_get_addrinfo(self, 
+									   host, 
+									   service, 
+									   CLIENT_SIDE_FLAGS, 
+									   &result);
 	
 	if (rt_code == SUCCESS) {
 		rt_code = _socket_addrinfo_iterate(self, result);
@@ -62,28 +76,33 @@ ssize_t socket_receive(socket_t *self, char *buffer, size_t length) {
 	return -1;
 }
 
-int _socket_is_in_valid_state(socket_t *self) {
+static int _socket_is_in_valid_state(socket_t *self) {
 	return (self != NULL && self->file_descriptor != ERROR);
 }
 
-int _socket_get_addrinfo(socket_t *self, 
-								 const char *host, 
-								 const char *service,
-								 const int flags, 
-								 struct addrinfo **result) {
+static int _socket_get_addrinfo(socket_t *self, 
+								const char *host, 
+								const char *service, 
+								const int flags, 
+								struct addrinfo **result) {
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = flags;
 
-	if (getaddrinfo(host, service, &hints, result) != ERROR) {
-		return SUCCESS;
+	int rt_code = getaddrinfo(host, service, &hints, result);
+	if (rt_code == SUCCESS) {
+		return rt_code;
 	}
-	return ERROR;
+	#ifdef DEBUG_INFO
+	DEBUG_PRINT("'getaddrinfo' in _socket_get_addrinfo:socket.c: %s\n", 
+				 gai_strerror(rt_code))
+	#endif
+	return rt_code;
 }
 
-int _socket_addrinfo_iterate(socket_t *self, struct addrinfo *result) {
+static int _socket_addrinfo_iterate(socket_t *self, struct addrinfo *result) {
 	int rt_code = ERROR;
 
 	while (result != NULL && rt_code == ERROR) {
@@ -92,13 +111,29 @@ int _socket_addrinfo_iterate(socket_t *self, struct addrinfo *result) {
 	return rt_code;
 }
 
-int _socket_create_and_connect(socket_t *self, struct addrinfo **result) {
-	int rt_code = 0;
-	self->file_descriptor = socket((*result)->ai_family, (*result)->ai_socktype, (*result)->ai_protocol);
-	rt_code = connect(self->file_descriptor, (*result)->ai_addr, (*result)->ai_addrlen);
-
+static int _socket_create_and_connect(socket_t *self, 
+									  struct addrinfo **result) {
+	int rt_code = ERROR;
+	self->file_descriptor = socket((*result)->ai_family, 
+								   (*result)->ai_socktype, 
+								   (*result)->ai_protocol);
+	#ifdef DEBUG_INFO
+	DEBUG_PRINT("'socket' in _socket_create_and_connect:socket.c: %s\n", 
+				 strerror(errno))
+	#endif
+	rt_code = connect(self->file_descriptor, 
+					  (*result)->ai_addr, 
+					  (*result)->ai_addrlen);
+	#ifdef DEBUG_INFO
+	DEBUG_PRINT("'connect' in _socket_create_and_connect:socket.c: %s\n", 
+				 strerror(errno))
+	#endif
 	if (rt_code == ERROR) {
 		close(self->file_descriptor);
+		#ifdef DEBUG_INFO
+		DEBUG_PRINT("'close' in _socket_create_and_connect:socket.c: %s\n", 
+					 strerror(errno))
+		#endif
 		*result = (*result)->ai_next;
 	}
 	return rt_code;
