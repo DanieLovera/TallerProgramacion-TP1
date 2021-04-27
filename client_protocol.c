70a54b9f0f29422324c03c6ecafb5909dce70903
Daniel Lovera <daanilo2497@gmail.com>
2021-04-27 15:26:29 -0300

    Refactor del protocolo cliente al agregarle el uso del protocolo de comunicacion comun y se agrega documentacion parcial

--- client_protocol.c
+++ client_protocol.c
@@ -1,13 +1,10 @@
 #include "client_protocol.h"
+#include "common_communication_protocol.h"
 #include <stdlib.h>
 #include <arpa/inet.h>
 
 #define SUCCESS 0
 #define ERROR -1
-
-void client_protocol_send(client_protocol_t *self, char *buffer, ssize_t size);
-void client_protocol_send_size(client_protocol_t *self, ssize_t size);
-void client_protocol_send_chunk(client_protocol_t *self, char *buffer, ssize_t size);
 
 int client_protocol_init(client_protocol_t *self, 
 						 const char *host, 
@@ -19,41 +16,27 @@ int client_protocol_init(client_protocol_t *self,
 }
 
 void client_protocol_run(client_protocol_t *self, FILE *file) {
+	comm_protocol_t comm_protocol;
+	comm_protocol_init(&comm_protocol, &(self->socket));
+	char *server_response = NULL; 
+	ssize_t response_size = 0;
 	
-	//ssize_t socket_send(socket_t *self, const void *buffer, size_t length)
-	//ssize_t socket_receive(socket_t *self, void *buffer, size_t length);
-
 	char *buffer = NULL;
 	size_t buffer_size = 0;
 	ssize_t read_bytes = getline(&buffer, &buffer_size, file);
 	while(read_bytes != EOF) {
-		client_protocol_send(self, buffer, read_bytes);
-		//client_protocol_send_size(self, read_bytes);
-		//client_protocol_send_chunk(self, buffer, read_bytes);
-		//fprintf(stdout, "%02x\n", buff[read_bytes-1]);
-		//fprintf(stdout, "%li\n", read_bytes);
+		comm_protocol_client_send(&comm_protocol, buffer, read_bytes);
+		printf("%s", buffer);
+		comm_protocol_client_receive(&comm_protocol, 
+									 &server_response, 
+									 &response_size);
+
 		read_bytes = getline(&buffer, &buffer_size, file);
 	}
+	comm_protocol_uninit(&comm_protocol);
 	free(buffer);
-
 }
 
 void client_protocol_uninit(client_protocol_t *self) {
 	socket_uninit(&(self->socket));
-}
-
-void client_protocol_send(client_protocol_t *self, char *buffer, ssize_t size) {
-	client_protocol_send_size(self, size);
-	client_protocol_send_chunk(self, buffer, size);
-}
-
-void client_protocol_send_size(client_protocol_t *self, ssize_t size) {
-	uint16_t bytes_to_send = (uint16_t)size;
-	bytes_to_send = htons(bytes_to_send);
-	printf("%04x\n", bytes_to_send);
-	socket_send(&(self->socket), (const void*)(&bytes_to_send), sizeof(uint16_t));
-}
-
-void client_protocol_send_chunk(client_protocol_t *self, char *buffer, ssize_t size) {
-	socket_send(&(self->socket), (const void*)buffer, size);
 }
--- client_protocol.h
+++ client_protocol.h
@@ -8,21 +8,25 @@ typedef struct client_protocol {
 	socket_t socket;
 } client_protocol_t;
 
+/**
+ * @brief Inicializa el protocolo del cliente.
+ * @param host: Dominio de la maquina en donde correra el cliente.
+ * @param service: Puerto de la maquina del cliente.
+ */
 int client_protocol_init(client_protocol_t *self, 
 						 const char *host, 
 						 const char *service);
+
+/**
+ * @brief Ejecuta el protocolo cliente.
+ * @param file: .
+ * @param service: Puerto de la maquina en el que se ubicara un socket.
+ */
 void client_protocol_run(client_protocol_t *self, FILE *file);
+
+/**
+ * @brief Libera los recursos del protocolo cliente.
+ */
 void client_protocol_uninit(client_protocol_t *self);
-
-	/*char *buff = NULL;
-	size_t buff_size = 0;
-	ssize_t bytes_read = getline(&buff, &buff_size, file);
-	while(bytes_read != EOF) {
-
-		//fprintf(stdout, "%02x\n", buff[bytes_read-1]);
-		//fprintf(stdout, "%li\n", bytes_read);
-		bytes_read = getline(&buff, &buff_size, file);
-	}	
-	free(buff);*/
 
 #endif
