6cb09d79eca59dd3d804a7ef879243b670151e13
Daniel Lovera <daanilo2497@gmail.com>
2021-04-26 23:08:35 -0300

    Se agrega documentacion. Y se finaliza por ahora el cifrado

--- hill_cipher.c
+++ hill_cipher.c
@@ -8,23 +8,84 @@
 #define SUCCESS 0
 #define ERROR -1
 
+/**
+ * @brief Inicializa el estado resultado del cipher. Si la longitud
+ * length es mayor a la del buffer el comportamiento sera indefinido.
+ * @param buffer: Guarda el contenido a encriptar.
+ * @param length: Guarda el tamanio del buffer a encriptar
+ */
 static void _hill_cipher_init_result(hill_cipher_t *self, 
 									 const unsigned char *buffer, 
-									size_t length);
+									 size_t length);
+
+/**
+ * @brief Inicializa el estado para la key del cipher. Si la longitud
+ * length es mayor a la del buffer el comportamiento sera indefinido.
+ * @param key: Guarda la key para el encriptado del algoritmo.
+ * @param length: Contiene el tamanio del buffer a encriptar
+ */
 static void _hill_cipher_init_key(hill_cipher_t *self, 
-								  const unsigned char* key, 
+								  const unsigned char *key, 
 								  size_t length);
+
+/**
+ * @brief Libera los recursos utilizados por el resultado del cipher.
+ */
 static void _hill_cipher_free_result(hill_cipher_t *self);
+
+/**
+ * @brief Libera los recursos utilizados por la key del cipher.
+ */
 static void _hill_cipher_free_key(hill_cipher_t *self);
+
+/**
+ * @brief Filtra los caracteres que no esten contenidos entre [A-Z], 
+ * modifica el contenido del buffer por el filtrado.
+ * @param buffer: Guarda el los caracteres que se analizaran para el
+ * filtrado.
+ * @param length: Guarda la longitud del buffer.
+ */
 static void _hill_cipher_filter(unsigned char *buffer, size_t *length);
+
+/**
+ * @brief Calcula la dimension que usa el algoritmo para encriptar y 
+ * la devuelve en el parametro dimension.
+ * @param dimension: Almacena la dimension del algoritmo.
+ */
 static void _hill_cipher_key_dimension(hill_cipher_t *self, size_t *dimension);
+
+/**
+ * @brief Calcula la nueva longitud del resultado si el filtrado
+ * disminuyo el tamanio de este y la devuelve en el parametro length.
+ * @param dimension: Almacena la dimension del algoritmo.
+ * @param length: Guarda la longitud del nuevo resultado.
+ */
 static void _hill_cipher_new_length(hill_cipher_t *self, 
 									size_t dimension, 
 									size_t *length);
+
+/**
+ * @brief Calcula las operaciones matematicas necesarias para
+ * el encriptado y modifica el resultado en el estado de self
+ * @param chunk: Contiene el buffer que se utiliza para
+ * operar con la key.
+ * @param dimension: Contiene la dimension de la key del encriptado.
+ */
 static void _hill_cipher_encode_math_ops(hill_cipher_t *self, 
 										 unsigned char *chunk, 
 										 size_t dimension);
+
+/**
+ * @brief Aplica los pasos del encriptado pos mapeo y los almacena
+ * en el resultado de self.
+ */
 static void _hill_cipher_encode(hill_cipher_t *self);
+
+/**
+ * @brief Coloca second_char en first char y * viceversa.
+ * @param first_char: Contiene el primer caracter.
+ * @param second_char: Contiene el segundo caracter.
+ */
 static void _hill_cipher_swap(unsigned char *first_char, 
 							  unsigned char *second_char);
 
@@ -37,19 +98,6 @@ static void _hill_cipher_swap(unsigned char *first_char,
 	printf("\n");
 }*/
 
-void hill_cipher_encode(hill_cipher_t *self, 
-						const unsigned char *buffer, 
-						size_t length) {
-	_hill_cipher_init_result(self, buffer, length);
-	hill_cipher_map(self->key, self->key_length);
-	hill_cipher_map(self->result, self->result_length);
-	_hill_cipher_filter(self->result, &(self->result_length));
-	_hill_cipher_encode(self);
-	hill_cipher_unmap(self->key, self->key_length);
-	hill_cipher_unmap(self->result, self->result_length);
-}
-
-
 void hill_cipher_init(hill_cipher_t *self, 
 					  const unsigned char *key, 
 					  size_t key_length) {
@@ -60,6 +108,18 @@ void hill_cipher_init(hill_cipher_t *self,
 void hill_cipher_uninit(hill_cipher_t *self) {
 	_hill_cipher_free_key(self);
 	_hill_cipher_free_result(self);
+}
+
+void hill_cipher_encode(hill_cipher_t *self, 
+						const unsigned char *buffer, 
+						size_t length) {
+	_hill_cipher_init_result(self, buffer, length);
+	hill_cipher_map(self->key, self->key_length);
+	hill_cipher_map(self->result, self->result_length);
+	_hill_cipher_filter(self->result, &(self->result_length));
+	_hill_cipher_encode(self);
+	hill_cipher_unmap(self->key, self->key_length);
+	hill_cipher_unmap(self->result, self->result_length);
 }
 
 static void _hill_cipher_init_result(hill_cipher_t *self, 
@@ -100,7 +160,6 @@ static void _hill_cipher_free_result(hill_cipher_t *self) {
 	}
 }
 
-
 static void _hill_cipher_encode(hill_cipher_t *self) {
 	size_t dimension = 0;
 	size_t result_length = 0;
@@ -117,7 +176,6 @@ static void _hill_cipher_encode(hill_cipher_t *self) {
 	_hill_cipher_encode_math_ops(self, aux, dimension);
 	free(aux);
 }
-
 
 static void _hill_cipher_key_dimension(hill_cipher_t *self, size_t *dimension) {
 	*dimension = sqrt(self->key_length);
--- hill_cipher.h
+++ hill_cipher.h
@@ -10,13 +10,31 @@ typedef struct hill_cipher {
 	size_t result_length;
 } hill_cipher_t;
 
+/**
+ * @brief Inicializa el cipher.
+ * @param key: Clave que se utiliza en el cifrado.
+ * @param key_length: Longitud de la clave.
+ */
 void hill_cipher_init(hill_cipher_t *self, 
 					  const unsigned char *key, 
 					  size_t key_length);
+
+/**
+ * @brief Encripta el mensaje del buffer y se guarda 
+ * el resultado en el estado.
+ * @param buffer: Valores a encriptar.
+ * @param Longitud de los valores a encriptar.
+ */
 void hill_cipher_encode(hill_cipher_t *self, 
 						const unsigned char *buffer, 
 						size_t length);
-void do_something(hill_cipher_t *self);
+
+/**
+ * @brief Libera los recursos utilizados por el cipher.
+ */
 void hill_cipher_uninit(hill_cipher_t *self);
+
+
+void do_something(hill_cipher_t *self);
 
 #endif
