4e6ca8df06ad220ef655e177ee36e4d1f619d830
Daniel Lovera <daanilo2497@gmail.com>
2021-04-29 12:22:24 -0300

    Refactor de biblioteca de mapeo, se eliminaron returns innecesarios de algunas funciones y se actualizo la documentacion.

--- common_maplib.c
+++ common_maplib.c
@@ -2,22 +2,24 @@
 
 #define IN_RANGE 0
 #define OUT_OF_RANGE -1
-#define INVALID_CHARACTER 0xff
+#define OUT_OF_RANGE_CHAR 0xFF
+#define MIN_CHAR 'A'
+#define MAX_CHAR 'Z'
+#define MIN_VALUE (MIN_CHAR - MIN_CHAR)
+#define MAX_VALUE (MAX_CHAR - MIN_CHAR)
 
 /**
  * @brief Funcion que mapea un caracter
  * entre 'A-Z' a '0-25' y en caso de otro caracter se mapea a 0xFF.
- * @return (NO DEBERIA RETORNAR NADA ES AL PEDO LUEGO REVISAR.)
  */
-static int hill_cipher_map_char(unsigned char *character);
+static void hill_cipher_map_char(unsigned char *character);
 
 /**
  * @brief Funcion que desmapea el valor
  * entre '0-25' a 'A-Z' y en caso de otro valor no se toma accion
  * alguna, la funcion no puede retornar el caracter original.
- * @return (NO DEBERIA RETORNAR NADA ES AL PEDO LUEGO REVISAR.)
  */
-static int hill_cipher_unmap_char(unsigned char *value);
+static void hill_cipher_unmap_char(unsigned char *value);
 
 /**
  * @brief Funcion que itera sobre todo el buffer y realiza la accion
@@ -26,50 +28,45 @@ static int hill_cipher_unmap_char(unsigned char *value);
  * @param length: Longitud del buffer.
  * @param callback: funcion que determina la accion sobre los 
  * caracteres del buffer.
- * @return (NO DEBERIA RETORNAR NADA ES AL PEDO LUEGO REVISAR.)
  */
-static int hill_cipher_iterate(unsigned char *buffer, 
+static void hill_cipher_iterate(unsigned char *buffer, 
 							   size_t length, 
-							   int (*callback)(unsigned char* character));
+							   void (*callback)(unsigned char* character));
 
-int hill_cipher_map(unsigned char *buffer, size_t length) {
-	return hill_cipher_iterate(buffer, length, hill_cipher_map_char);
+void hill_cipher_map(unsigned char *buffer, size_t length) {
+	hill_cipher_iterate(buffer, length, hill_cipher_map_char);
 }
 
-int hill_cipher_unmap(unsigned char *buffer, size_t length) {
-	return hill_cipher_iterate(buffer, length, hill_cipher_unmap_char);
+void hill_cipher_unmap(unsigned char *buffer, size_t length) {
+	hill_cipher_iterate(buffer, length, hill_cipher_unmap_char);
 }
 
 int hill_cipher_is_valid_value(const unsigned char character) {
-	if (character == INVALID_CHARACTER) {
+	if (character == OUT_OF_RANGE_CHAR || 
+		(character > MAX_VALUE || character < MIN_VALUE)) {
 		return OUT_OF_RANGE;
 	}
 	return IN_RANGE;
 }
 
-static int hill_cipher_map_char(unsigned char *character) {
-	if (*character >= 'A' && *character <= 'Z') {
-		*character = *character - 'A';
-		return IN_RANGE;
-	} 
-	*character = INVALID_CHARACTER;
-	return OUT_OF_RANGE;
-}
-
-static int hill_cipher_unmap_char(unsigned char *value) {
-	if (*value >= ('A'-'A') && *value <= ('Z'-'A')) {
-		*value = *value + 'A';
-		return IN_RANGE;
+static void hill_cipher_map_char(unsigned char *character) {
+	if (*character >= MIN_CHAR && *character <= MAX_CHAR) {
+		*character = *character - MIN_CHAR;
+	} else {
+		*character = OUT_OF_RANGE_CHAR;
 	}
-	return OUT_OF_RANGE;
 }
 
-static int hill_cipher_iterate(unsigned char *buffer, 
-							   size_t length, 
-							   int (*callback)(unsigned char* character)) {
-	int status = IN_RANGE;
+static void hill_cipher_unmap_char(unsigned char *value) {
+	if (*value >= (MIN_CHAR - MIN_CHAR) && *value <= (MAX_CHAR - MIN_CHAR)) {
+		*value = *value + MIN_CHAR;
+	}
+}
+
+static void hill_cipher_iterate(unsigned char *buffer, 
+							   	size_t length, 
+							   	void (*callback)(unsigned char* character)) {
 	for (int i = 0; i < length; i++) {
-		status = callback(&(buffer[i]));
+		callback(&(buffer[i]));
 	}
-	return status;
 }
--- common_maplib.h
+++ common_maplib.h
@@ -7,21 +7,20 @@
  * @brief Funcion que mapea los caracteres del buffer
  * entre 'A-Z' a '0-25' y en caso de otro caracter se mapea a 0xFF.
  * @param length: Longitud del buffer.
- * @return (NO DEBERIA RETORNAR NADA ES AL PEDO LUEGO REVISAR.)
  */
-int hill_cipher_map(unsigned char *buffer, size_t length);
+void hill_cipher_map(unsigned char *buffer, size_t length);
 
 /**
  * @brief Funcion que desmapea los caracteres del buffer
  * entre '0-25' a 'A-Z' y en caso de otro caracter no se toma accion
  * alguna, la funcion no puede retornar el caracter original.
  * @param lenght: Longitud del buffer.
- * @return (NO DEBERIA RETORNAR NADA ES AL PEDO LUEGO REVISAR.)
  */
-int hill_cipher_unmap(unsigned char *buffer, size_t length);
+void hill_cipher_unmap(unsigned char *buffer, size_t length);
 
 /**
  * @brief Valida el caracter de entrada
+ * @param character: valor a validar.
  * @return Devuelve 0 en caso de que el caracter pertenezca a 'A-Z' 
  * o -1 en caso contrario. (REVISAR EL CASO -1 TENGO UN ERROR SI
  * EL CHARACTER ES CUALQUIERA FUERA DEL RANGO)
